aefb5ee85a334f21d82088a65b7370eb1bd8276a
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-07-17 18:20:57 +0100

    Create an assignment component

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,17 +1,17 @@
+import Assignment from "./Assignment";
+
 export default {
+    components: { Assignment },
     template: `
         <section v-show="assignments.length">
             <h2 class="font-bold mb-2">{{ title }}</h2>
             <ul>
-                <li
+                <assignment
                     v-for="assignment in assignments"
                     :key="assignment.id"
+                    :assignment="assignment"
                 >
-                    <label>
-                        {{ assignment.name }}
-                        <input type="checkbox" v-model="assignment.complete">
-                    </label>
-                </li>
+                </assignment>
             </ul>
         </section>
     `,
