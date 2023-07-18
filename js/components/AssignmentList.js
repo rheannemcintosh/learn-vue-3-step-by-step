0117e3cab0eef0d80cb1274631d15b1f4399c748
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-07-18 20:36:31 +0100

    Implement tag functionality for the assignments list

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -10,12 +10,21 @@ export default {
             </h2>
             
             <div class="flex gap-2">
-                <button v-for="tag in tags" class=" border rounded px-1 py-px text-xs">{{ $tag }}</button>
+                <button 
+                    v-for="tag in tags"
+                    @click="currentTag = tag"
+                    class=" border rounded px-1 py-px text-xs"
+                    :class="{
+                        'border-blue-500 text-blue-500' : tag === currentTag
+                    }"
+                >
+                    {{ $tag }}
+                </button>
             </div>
             
             <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignment
-                    v-for="assignment in assignments"
+                    v-for="assignment in filteredAssignments"
                     :key="assignment.id"
                     :assignment="assignment"
                 >
@@ -29,9 +38,23 @@ export default {
         title: String,
     },
 
+    data() {
+        return {
+            currentTag: 'all'
+        };
+    },
+
     computed: {
+        filteredAssignments() {
+            if (this.currentTag === 'all') {
+                return this.assignments;
+            }
+
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        },
+
         tags() {
-            return ['science', 'math', 'reading']
+            return ['all', ...new Set(this.assignments.map(a => a.tag))];
         }
     }
 }
