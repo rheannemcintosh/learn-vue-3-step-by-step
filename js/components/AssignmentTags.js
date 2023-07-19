33b5fdba22200ca48648b9c0b1864cc1e839d1ac
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-07-20 00:11:37 +0100

    Fix bug with tags

--- js/components/AssignmentTags.js
+++ js/components/AssignmentTags.js
@@ -1,15 +1,15 @@
 export default {
     template: `
         <div class="flex gap-2">
-            <button 
-                v-for="tag in tags"
+            <button
                 @click="$emit('update:currentTag', tag)"
+                v-for="tag in tags"
                 class=" border rounded px-1 py-px text-xs"
                 :class="{
                     'border-blue-500 text-blue-500' : tag === currentTag
                 }"
             >
-                {{ $tag }}
+                {{ tag }}
             </button>
         </div>
     `,
