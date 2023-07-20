d8324de74437eb1cf49b4200874d78c007ce5c76
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-07-20 22:07:55 +0100

    Add a footer slot

--- js/components/App.js
+++ js/components/App.js
@@ -9,10 +9,24 @@ export default {
             <assignments></assignments>
             <panel>
                 <template v-slot:heading>
-                    Hi there
+                    Hi I have a heading
                 </template>
                 
                 This is my default content
+            </panel>
+            <panel>                
+                This is my default content
+            </panel>
+            <panel>
+                <template v-slot:heading>
+                    Hi I have a footer
+                </template>
+                
+                This is my default content
+                
+                <template v-slot:footer>
+                    Click Here to Learn More
+                </template>
             </panel>
         </div>
     `,
--- js/components/Panel.js
+++ js/components/Panel.js
@@ -1,11 +1,16 @@
 export default {
     template: `
         <div class="bg-gray-700 p-4 border border-gray-600 rounded-lg">
-            <h2 class="font-bold">
+            <h2 v-if="$slots.heading" class="font-bold mb-2">
                 <slot name="heading" />
             </h2>
             
             <slot />
+            
+            <footer v-of="$slots.footer" class="border-gray-600 border-t mt-4 pt-4 text-sm">
+                 <slot name="footer" />
+            </footer>   
+           
         </div>
     `,
 }
