6213a0077fbbc47eee75e563db9497133738ac7c
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-07-20 22:19:16 +0100

    Add panel to the assignment lists

--- js/components/App.js
+++ js/components/App.js
@@ -7,38 +7,6 @@ export default {
     template: `
         <div class="grid gap-6">
             <assignments></assignments>
-            <panel>
-                <template v-slot:heading>
-                    Hi I have a heading
-                </template>
-                
-                This is my default content
-            </panel>
-            <panel>                
-                This is my default content
-            </panel>
-            <panel>
-                <template v-slot:heading>
-                    Hi I have a footer
-                </template>
-                
-                This is my default content
-                
-                <template v-slot:footer>
-                    Click Here to Learn More
-                </template>
-            </panel>
-            <panel theme="light">
-                <template v-slot:heading>
-                    Hi I have a footer
-                </template>
-                
-                This is my default content
-                
-                <template v-slot:footer>
-                    Click Here to Learn More
-                </template>
-            </panel>
         </div>
     `,
 }
