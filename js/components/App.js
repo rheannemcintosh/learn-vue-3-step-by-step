6ecfd4cc8f222914767fbd55e97ffbfb9cfc04ef
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-07-20 21:55:54 +0100

    Add a new card to App.js

--- js/components/App.js
+++ js/components/App.js
@@ -4,6 +4,11 @@ export default {
     components: { Assignments },
 
     template: `
-        <assignments></assignments>
+        <div class="grid gap-6">
+            <assignments></assignments>
+            <div class="bg-gray-700 p-4 border border-gray-600 rounded-lg">
+                <h2 class="font-bold">Hello World</h2>
+            </div>
+        </div>
     `,
 }
